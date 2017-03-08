1ab3aa77dec86ce1c911b6610cdac788e8de8811
ngot <zhuanghengfei@gmail.com>
2017-03-08 11:48:08 +0800

    feat:support appveyor service

--- test/install.test.js
+++ test/install.test.js
@@ -126,10 +126,15 @@ test('generate LICENSE with object', t => {
 test('generate service with string', t => {
   const env = Object.assign({}, process.env, { CI_ROOT_FOR_TEST: getRoot('service') });
   execSync(cmd, { env, stdout: [ 0, 1, 2 ] });
-  const file = fs.readFileSync(getRoot('service/.travis.yml'), 'utf8');
+  let file = fs.readFileSync(getRoot('service/.travis.yml'), 'utf8');
   t.regex(file, /services/);
   t.regex(file, /- redis-server/);
   t.regex(file, /- mysql/);
+  file = fs.readFileSync(getRoot('service/appveyor.yml'), 'utf8');
+  t.regex(file, /services/);
+  t.regex(file, /- redis-server/);
+  t.regex(file, /- mysql/);
+  t.regex(file, /redis-server.exe/);
 });
 
 function getRoot(name) {
